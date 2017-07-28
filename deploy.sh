f9ecd8341b2e15215de9b3d261149766d0ad04fc
Elnar Ibragimov <djelnarbc2@gmail.com>
2017-07-28 22:05:45 +0500

    f

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@ ssh root@82.202.212.41 '
     cd /root/adidas-shop;
     git pull;
     npm install;
-    npm run build;
     forever stop server.js;
+    webpack -p;
     forever start server.js
 '
--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const router = require('koa-router')()
 
 router.get('/', async ctx => {
     await ctx.render('index')
+    console.log(ctx.status)
 })
 
 server
